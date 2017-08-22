f5112338bfbb5515ade3b871fd561e1e187afee9
deakzsolt <zsoltd@4choice.com>
2017-08-22 22:07:08 +0200

    Initial setup for the install script

---
+++ os/0.0.1/install/controllers/install.controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deakzsolt
+ * Date: 2017. 08. 22.
+ * Time: 21:54
+ */
+
+class InstallController extends Controller {
+
+	function view() {
+		$this->set('title','Installation');
+	} // view
+}
